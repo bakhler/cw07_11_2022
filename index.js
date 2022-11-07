daa0bd2ee4dfdd8191365613bcceb86d3b0caaa6
bakhler <thehenridit@gmail.com>
2022-11-07 10:02:50 +0200

    Add let

---
+++ index.js
@@ -0,0 +1,14 @@
+let number1;
+console.log(number1);
+number1 = 12;
+console.log(number1);
+
+let userFirstName;
+
+const userFirstName = 'Pitt'
+
+userFirstName = 'Brad';
+
+userLastName = 'Cruse';
+
+console.log(userFirstName, userLastName)
